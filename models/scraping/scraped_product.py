2fe2fa0f36e7489eb87e96e8163c5fbd963337db
Cesar Sanchez <impomerchgmail.com>
2026-07-29 22:57:26 -0500

    Crear modelo base para productos obtenidos por scraping

---
+++ models/scraping/scraped_product.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ScrapedProduct:
+
+    source: str
+    url: str
+
+    code: str = ""
+    name: str = ""
+    category: str = ""
+
+    price: float = 0.0
+
+    image_url: str = ""
+
+    description: str = ""

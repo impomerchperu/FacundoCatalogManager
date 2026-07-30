7e662a89528f742e6fe959537bb3f2f14cca0298
Cesar Sanchez <impomerchgmail.com>
2026-07-29 23:07:40 -0500

    Crear parser HTML base para web scraping

---
+++ scrapers/parser.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+
+class Parser:
+
+    def parse(self, html):
+
+        soup = BeautifulSoup(
+            html,
+            "lxml"
+        )
+
+        return soup

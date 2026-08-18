abf859c9f0b15b8189fea29de8f0c76d19e5105a
impomerchperu <impomerch@gmail.com>
2026-08-17 23:22:03 -0500

    test(scraping): cover embedded JetSmartFilters pagination

--- tests/test_category_scraper.py
+++ tests/test_category_scraper.py
@@ -16,3 +16,36 @@ def test_category_scraper_extracts_categories():
     result = scraper.scrape("https://example.com/categories")
 
     assert result == ["Herramientas", "Electricidad"]
+
+
+def test_category_scraper_detects_embedded_jetsmartfilters_pages():
+    html = """
+    <html>
+      <body>
+        <div class="jet-filters-pagination__item" data-value="1">
+          <div class="jet-filters-pagination__link">1</div>
+        </div>
+        <div class="jet-filters-pagination__item" data-value="2">
+          <div class="jet-filters-pagination__link">2</div>
+        </div>
+        <div class="jet-filters-pagination__item" data-value="3">
+          <div class="jet-filters-pagination__link">3</div>
+        </div>
+      </body>
+    </html>
+    """
+
+    class FakeBrowser:
+        def get(self, url):
+            return html
+
+    scraper = CategoryScraper(FakeBrowser())
+    category_url = "https://example.com/categoria-producto/jarros-mug/"
+
+    pages = scraper.get_category_pages(category_url)
+
+    assert pages == [
+        category_url,
+        "https://example.com/categoria-producto/jarros-mug/page/2/",
+        "https://example.com/categoria-producto/jarros-mug/page/3/",
+    ]

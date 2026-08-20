398b47a0ce939b36fc9c2d7099b17c6b9c92264f
impomerchperu <impomerch@gmail.com>
2026-08-19 20:57:00 -0500

    test: cover invalid embedded pagination recovery

--- tests/test_category_scraper.py
+++ tests/test_category_scraper.py
@@ -1,3 +1,5 @@
+import requests
+
 from scrapers.collectors.category_scraper import CategoryScraper
 
 
@@ -49,3 +51,33 @@ def test_category_scraper_detects_embedded_jetsmartfilters_pages():
         "https://example.com/categoria-producto/jarros-mug/page/2/",
         "https://example.com/categoria-producto/jarros-mug/page/3/",
     ]
+
+
+def test_category_scraper_skips_invalid_embedded_page_without_aborting_category():
+    category_url = "https://example.com/categoria-producto/jarros-mug/"
+    category_html = """
+    <div class="jet-filters-pagination__item" data-value="2">
+      <div class="jet-filters-pagination__link">2</div>
+    </div>
+    <div class="jet-filters-pagination__item" data-value="3">
+      <div class="jet-filters-pagination__link">3</div>
+    </div>
+    """
+    page_3_url = f"{category_url}page/3/"
+    page_3_html = "<div class=\"product-page\">productos pagina 3</div>"
+
+    class FakeBrowser:
+        def get(self, url):
+            if url == category_url:
+                return category_html
+            if url == f"{category_url}page/2/":
+                raise requests.HTTPError("404 Not Found")
+            if url == page_3_url:
+                return page_3_html
+            raise AssertionError(f"URL inesperada: {url}")
+
+    scraper = CategoryScraper(FakeBrowser())
+
+    pages = scraper.get_category_pages(category_url)
+
+    assert pages == [category_url, f"{category_url}page/2/", page_3_url]

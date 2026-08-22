af2dcb773e47cc538ff2e6830bc6bb149d40ceff
impomerchperu <impomerch@gmail.com>
2026-08-22 00:29:24 -0500

    test(scraping): cover expected JetSmartFilters page count

--- tests/test_category_scraper.py
+++ tests/test_category_scraper.py
@@ -130,3 +130,39 @@ def test_category_scraper_does_not_probe_wordpress_when_jsf_metadata_exists():
     assert scraper.get_category_pages(category_url) == [category_url]
     assert browser.get_calls == [category_url]
     assert len(browser.post_calls) == 1
+
+
+def test_category_scraper_uses_expected_count_to_cover_all_pages():
+    category_url = (
+        "https://stock.importacionesfacundo.com/"
+        "categoria-producto/catalogo/"
+    )
+    responses = {
+        category_url: '<body class="tax-product_cat term-127"></body>',
+        "ajax:1": (
+            '{"found_posts":51,"max_num_pages":2,'
+            '"rendered_content":"<div>page1</div>"}'
+        ),
+        "ajax:2": (
+            '{"found_posts":51,"max_num_pages":2,'
+            '"rendered_content":"<div>page2</div>"}'
+        ),
+        "ajax:3": (
+            '{"found_posts":51,"max_num_pages":2,'
+            '"rendered_content":"<div>page3</div>"}'
+        ),
+    }
+    browser = FakeBrowser(responses)
+    scraper = CategoryScraper(browser)
+
+    pages = scraper.get_category_pages(category_url)
+
+    assert pages == [
+        category_url,
+        f"{category_url.rstrip('/')}?product-page=2",
+        f"{category_url.rstrip('/')}?product-page=3",
+    ]
+    assert [
+        next(value for key, value in data if key == "paged")
+        for _, data in browser.post_calls
+    ] == ["1", "2", "3"]

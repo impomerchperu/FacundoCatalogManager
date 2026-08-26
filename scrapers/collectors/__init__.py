6510a5d1d8527fa145caad8ef0864ccfe9b7e498
impomerchperu <impomerch@gmail.com>
2026-08-26 00:31:45 -0500

    refactor: remove category scraper monkey patch activation

--- scrapers/collectors/__init__.py
+++ scrapers/collectors/__init__.py
@@ -1,4 +1,1 @@
 """Collectors package initialization."""
-
-# Apply the JSF pagination correction before callers import CategoryScraper.
-from . import category_pagination_patch as _category_pagination_patch  # noqa: F401

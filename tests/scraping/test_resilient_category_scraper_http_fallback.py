605e58cee91c7edb6e6ff9c853a85d9232fd602e
impomerchperu <impomerch@gmail.com>
2026-09-04 01:11:25 -0500

    test(scraping): cover JSF HTTP fallback to category HTML

---
+++ tests/scraping/test_resilient_category_scraper_http_fallback.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import requests
+
+from scrapers.collectors.resilient_category_scraper import ResilientCategoryScraper
+
+
+CATEGORY_URL = "https://stock.importacionesfacundo.com/categoria-producto/enmicadoras-laminadoras/"
+CATEGORY_HTML = """
+<html>
+  <body class="archive tax-product_cat term-123">
+    <article>FB-1504</article>
+    <article>FB-1503</article>
+    <nav class="woocommerce-pagination">
+      <a href="/categoria-producto/enmicadoras-laminadoras/page/2/">2</a>
+    </nav>
+  </body>
+</html>
+"""
+
+
+class FailingJsfBrowser:
+    def __init__(self) -> None:
+        self.get_calls = 0
+        self.post_calls = 0
+
+    def get(self, url: str):
+        del url
+        self.get_calls += 1
+        return CATEGORY_HTML
+
+    def post(self, url: str, data=None):
+        del url, data
+        self.post_calls += 1
+        response = requests.Response()
+        response.status_code = 500
+        response.url = "https://stock.importacionesfacundo.com/wp-admin/admin-ajax.php"
+        raise requests.HTTPError(response=response)
+
+
+def test_jsf_http_error_falls_back_to_category_html():
+    browser = FailingJsfBrowser()
+    scraper = ResilientCategoryScraper(browser=browser)
+
+    pages = scraper.get_category_pages(CATEGORY_URL)
+
+    assert pages == [
+        CATEGORY_URL,
+        f"{CATEGORY_URL}page/2/",
+    ]
+    assert browser.get_calls >= 2
+    assert browser.post_calls >= 1

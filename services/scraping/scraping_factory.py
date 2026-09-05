b97f354f1d94549a68268b4ff553d60b6159b7a5
impomerchperu <impomerch@gmail.com>
2026-09-05 00:29:20 -0500

    style(scraping): organize scraping factory imports

--- services/scraping/scraping_factory.py
+++ services/scraping/scraping_factory.py
@@ -16,49 +16,29 @@ from scrapers.collectors.product_collection_scraper import (
 from scrapers.collectors.resilient_category_scraper import (
     ResilientCategoryScraper,
 )
-from scrapers.extractors.category_extractor import (
-    CategoryExtractor,
-)
+from scrapers.extractors.category_extractor import CategoryExtractor
 from scrapers.extractors.category_product_extractor import (
     CategoryProductExtractor,
 )
-from scrapers.extractors.product_card_extractor import (
-    ProductCardExtractor,
-)
+from scrapers.extractors.product_card_extractor import ProductCardExtractor
 from scrapers.extractors.product_extractor import ProductExtractor
-from services.scraping.catalog_sync_service import (
-    CatalogSyncService,
-)
+from services.scraping.catalog_sync_service import CatalogSyncService
 from services.scraping.category_product_scraping_service import (
     CategoryProductScrapingService,
 )
+from services.scraping.category_service import CategoryService
+from services.scraping.image_sync_adapter import ImageSyncAdapter
 from services.scraping.normalized_category_product_sync_service import (
     NormalizedCategoryProductSyncService,
 )
-from services.scraping.category_service import (
-    CategoryService,
-)
-from services.scraping.image_sync_adapter import (
-    ImageSyncAdapter,
-)
-from services.scraping.product_diff_service import (
-    ProductDiffService,
-)
-from services.scraping.scraped_product_mapper import (
-    ScrapedProductMapper,
-)
+from services.scraping.product_diff_service import ProductDiffService
+from services.scraping.scraped_product_mapper import ScrapedProductMapper
 from services.scraping.scraped_product_persistence_service import (
     ScrapedProductPersistenceService,
 )
-from services.scraping.scraping_config import (
-    ScrapingConfig,
-)
-from services.scraping.scraping_result_writer import (
-    ScrapingResultWriter,
-)
-from services.scraping.scraping_runner import (
-    ScrapingRunner,
-)
+from services.scraping.scraping_config import ScrapingConfig
+from services.scraping.scraping_result_writer import ScrapingResultWriter
+from services.scraping.scraping_runner import ScrapingRunner
 
 
 class ScrapingFactory:

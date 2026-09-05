8ff0211808b7eedca2ae3c78896a80c44dd8498b
impomerchperu <impomerch@gmail.com>
2026-09-05 03:47:56 -0500

    style(scraping): sort collectors compatibility imports

--- scrapers/collectors/__init__.py
+++ scrapers/collectors/__init__.py
@@ -6,22 +6,22 @@
 # recovery, price-detail, and JetSmartFilters concurrency behaviors.
 from . import category_pagination_patch as _category_pagination_patch
 from . import jsf_concurrency_patch as _jsf_concurrency_patch
+from . import jsf_first_page_recovery_patch as _jsf_first_page_recovery_patch
 from . import missing_code_recovery_patch as _missing_code_recovery_patch
 from . import page_coverage_recovery_patch as _page_coverage_recovery_patch
 from . import page_metrics_patch as _page_metrics_patch
 from . import price_detail_recovery_patch as _price_detail_recovery_patch
 from . import product_code_patch as _product_code_patch
 from . import scraping_compat as _scraping_compat
-from . import jsf_first_page_recovery_patch as _jsf_first_page_recovery_patch
 
 __all__ = [
     "_category_pagination_patch",
     "_jsf_concurrency_patch",
+    "_jsf_first_page_recovery_patch",
     "_missing_code_recovery_patch",
     "_page_coverage_recovery_patch",
     "_page_metrics_patch",
     "_price_detail_recovery_patch",
     "_product_code_patch",
     "_scraping_compat",
-    "_jsf_first_page_recovery_patch",
 ]

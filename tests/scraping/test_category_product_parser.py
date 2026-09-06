80dbb5934a2c263e145c90bf12b07970241cfe54
impomerchperu <impomerch@gmail.com>
2026-09-06 18:13:05 -0500

    test(scraping): migrate category parser coverage to ProductExtractor

--- tests/scraping/test_category_product_parser.py
+++ tests/scraping/test_category_product_parser.py
@@ -1,64 +1,36 @@
 from bs4 import BeautifulSoup
 
-from scrapers.parser.category_product_parser import (
-    CategoryProductParser,
-)
+from scrapers.extractors.product_extractor import ProductExtractor
 
 
-def test_category_product_parser_extracts_product():
-
+def test_product_extractor_replaces_legacy_category_product_parser_coverage():
     html = """
     <div class="jsfb-query--querymovil jsfb-filterable">
-
         <p>FB-1812</p>
-
-        <h2>
-            Taza de Plástico
-        </h2>
-
-        <img
-            data-src="https://site.com/FB-1812.webp"
-        >
-
+        <h2>Taza de Plástico</h2>
+        <img data-src="https://site.com/FB-1812.webp">
         <div class="content-precio">
             <h3>Precio Muestra</h3>
             <h4>S/ 6.50</h4>
         </div>
-
         <div class="content-precio">
             <h3>Precio Ciento</h3>
             <h4>S/ 520.00</h4>
         </div>
-
         <div class="content-precio">
             <h3>Precio Millar</h3>
             <h4>S/ 5000.00</h4>
         </div>
-
     </div>
     """
 
-    soup = BeautifulSoup(
-        html,
-        "html.parser",
-    )
-
-    block = soup.select_one(".jsfb-query--querymovil")
-
-    parser = CategoryProductParser()
-
-    product = parser.parse(block)
+    soup = BeautifulSoup(html, "lxml")
+    product = ProductExtractor().extract(soup)
 
     assert product is not None
-
     assert product.code == "FB-1812"
-
     assert product.name == "Taza de Plástico"
-
     assert product.price_sample == 6.50
-
     assert product.price_hundred == 520
-
     assert product.price_thousand == 5000
-
     assert product.image_url == "https://site.com/FB-1812.webp"

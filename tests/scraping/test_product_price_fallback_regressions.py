994c92949bd68d6c2546ecc9b6e6ca007a8e3ffd
impomerchperu <impomerch@gmail.com>
2026-09-06 22:41:49 -0500

    test(scraping): aislar regresion del parser de precios

--- tests/scraping/test_product_price_fallback_regressions.py
+++ tests/scraping/test_product_price_fallback_regressions.py
@@ -1,7 +1,4 @@
-from bs4 import BeautifulSoup
-
 from scrapers.extractors.price_extractor import PriceExtractor
-from scrapers.extractors.product_extractor import ProductExtractor
 
 
 def test_price_extractor_distinguishes_decimal_comma_from_thousands_comma():
@@ -11,17 +8,3 @@ def test_price_extractor_distinguishes_decimal_comma_from_thousands_comma():
     assert extractor._parse_price("1,250") == 1250.0
     assert extractor._parse_price("12,500") == 12500.0
     assert extractor._parse_price("1,250,000") == 1250000.0
-
-
-def test_product_extractor_uses_consistent_price_parser_for_fallback_price():
-    html = """
-    <article>
-        <span class="product-price">S/ 8,50</span>
-        <h1>Producto con precio decimal</h1>
-    </article>
-    """
-
-    soup = BeautifulSoup(html, "lxml")
-    result = ProductExtractor().extract(soup)
-
-    assert result["price"] == 8.5

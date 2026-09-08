c9ded57c487bb069a1a7d86ea1f3d8cbd85928d7
impomerchperu <impomerch@gmail.com>
2026-09-07 23:59:01 -0500

    fix: sort collector patch imports

--- scrapers/collectors/__init__.py
+++ scrapers/collectors/__init__.py
@@ -5,23 +5,23 @@
 # category pagination, WooCommerce code-discovery, page-audit, coverage
 # recovery, price-detail, and JetSmartFilters concurrency behaviors.
 from . import category_pagination_patch as _category_pagination_patch
-from . import recursive_pagination_patch as _recursive_pagination_patch
 from . import jsf_concurrency_patch as _jsf_concurrency_patch
 from . import missing_code_recovery_patch as _missing_code_recovery_patch
 from . import page_coverage_recovery_patch as _page_coverage_recovery_patch
 from . import page_metrics_patch as _page_metrics_patch
 from . import price_detail_recovery_patch as _price_detail_recovery_patch
 from . import product_code_patch as _product_code_patch
+from . import recursive_pagination_patch as _recursive_pagination_patch
 from . import scraping_compat as _scraping_compat
 
 __all__ = [
     "_category_pagination_patch",
-    "_recursive_pagination_patch",
     "_jsf_concurrency_patch",
     "_missing_code_recovery_patch",
     "_page_coverage_recovery_patch",
     "_page_metrics_patch",
     "_price_detail_recovery_patch",
     "_product_code_patch",
+    "_recursive_pagination_patch",
     "_scraping_compat",
 ]

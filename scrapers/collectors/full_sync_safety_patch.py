acd697a4b9bdffbe6d0d6aeb240dce07e109dd18
impomerchperu <impomerch@gmail.com>
2026-09-11 23:38:25 -0500

    fix: type dynamic full-sync safety hooks

--- scrapers/collectors/full_sync_safety_patch.py
+++ scrapers/collectors/full_sync_safety_patch.py
@@ -128,9 +128,9 @@ def activate() -> None:
     global _PATCHED
     if _PATCHED:
         return
-    CategoryProductSyncService.sync_categories = _sync_categories_with_safety
-    CategoryProductSyncService._full_sync_prune_guard = _coverage_guard_with_state
-    CategoryProductSyncService.sync_products = _sync_products_with_safety
+    setattr(CategoryProductSyncService, "sync_categories", _sync_categories_with_safety)
+    setattr(CategoryProductSyncService, "_full_sync_prune_guard", _coverage_guard_with_state)
+    setattr(CategoryProductSyncService, "sync_products", _sync_products_with_safety)
     _PATCHED = True
 
 

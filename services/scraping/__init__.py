139fd11fc752357438ff7915e8e97688d6dbc632
impomerchperu <impomerch@gmail.com>
2026-09-13 00:20:48 -0500

    refactor: retire category coverage monkey patch

--- services/scraping/__init__.py
+++ services/scraping/__init__.py
@@ -6,7 +6,6 @@ repositories can import individual scraping helpers without circular imports.
 
 from typing import TYPE_CHECKING
 
-from . import category_coverage_patch as _category_coverage_patch
 from . import prune_guard_recovery_patch as _prune_guard_recovery_patch
 from .catalog_sync_service import CatalogSyncService
 from .category_product_scraping_service import CategoryProductScrapingService
@@ -24,7 +23,6 @@ from .scraping_session import ScrapingSession, ScrapingSessionResult
 if TYPE_CHECKING:
     from .scraping_factory import ScrapingFactory
 
-_category_coverage_patch.activate()
 _prune_guard_recovery_patch.activate()
 
 __all__ = [

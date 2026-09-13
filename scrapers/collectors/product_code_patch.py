06b0e71fb8f2991c7a05d06517fded795366e554
impomerchperu <impomerch@gmail.com>
2026-09-13 00:35:59 -0500

    chore: replace safe setattr calls in product code patch

--- scrapers/collectors/product_code_patch.py
+++ scrapers/collectors/product_code_patch.py
@@ -145,18 +145,10 @@ def activate() -> None:
     if _PATCHED:
         return
 
-    setattr(ProductExtractor, "_legacy_extract_code", _ORIGINAL_EXTRACT_CODE)
-    setattr(ProductExtractor, "extract_code", _extract_code)
-    setattr(
-        ProductCollectionScraper,
-        "_enrich_from_detail_page",
-        _enrich_with_authoritative_code,
-    )
-    setattr(
-        CategoryProductExtractor,
-        "_normalize_code",
-        classmethod(_normalize_category_code),
-    )
+    ProductExtractor._legacy_extract_code = _ORIGINAL_EXTRACT_CODE
+    ProductExtractor.extract_code = _extract_code
+    ProductCollectionScraper._enrich_from_detail_page = _enrich_with_authoritative_code
+    CategoryProductExtractor._normalize_code = classmethod(_normalize_category_code)
     _PATCHED = True
 
 

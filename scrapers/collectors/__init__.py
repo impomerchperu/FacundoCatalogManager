c189384076034f611120945a79b0c28f6bbca7fe
impomerchperu <impomerch@gmail.com>
2026-09-13 23:22:20 -0500

    refactor(scraping): stop importing retired JSF patch

--- scrapers/collectors/__init__.py
+++ scrapers/collectors/__init__.py
@@ -1,12 +1,10 @@
 """Collectors package initialization."""
 
 # Only runtime compatibility layers that still add behavior are activated here.
-from . import jsf_concurrency_patch as _jsf_concurrency_patch
 from . import page_metrics_patch as _page_metrics_patch
 from . import price_detail_recovery_patch as _price_detail_recovery_patch
 
 __all__ = [
-    "_jsf_concurrency_patch",
     "_page_metrics_patch",
     "_price_detail_recovery_patch",
 ]

09149bc0d9c6edc87e891415ab6b481cef8e57bb
impomerchperu <impomerch@gmail.com>
2026-09-13 23:32:34 -0500

    refactor(scraping): stop importing retired price recovery patch

--- scrapers/collectors/__init__.py
+++ scrapers/collectors/__init__.py
@@ -1,8 +1,1 @@
 """Collectors package initialization."""
-
-# Only runtime compatibility layers that still add behavior are activated here.
-from . import price_detail_recovery_patch as _price_detail_recovery_patch
-
-__all__ = [
-    "_price_detail_recovery_patch",
-]

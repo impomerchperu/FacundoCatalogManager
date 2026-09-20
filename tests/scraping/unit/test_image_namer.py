37df5adfec2d58fbd40d67283efab00b393fe9a1
impomerchperu <impomerch@gmail.com>
2026-09-19 22:51:16 -0500

    test(images): normalize module-level assertions

--- tests/scraping/unit/test_image_namer.py
+++ tests/scraping/unit/test_image_namer.py
@@ -1,50 +1,36 @@
 from scrapers.images.image_namer import ImageNamer
 
-namer = ImageNamer()
 
+def test_image_namer_builds_expected_paths():
+    namer = ImageNamer()
 
-tests = [
-    (
-        "FB-1812",
-        "https://site.com/images/FB-1812.webp",
-        "data/images/FB-1812.webp",
-    ),
-    (
-        "FB-1800-AZ",
-        "https://site.com/a/b/c.png",
-        "data/images/FB-1800-AZ.png",
-    ),
-    (
-        "ABC123",
-        "https://site.com/img/test.jpg",
-        "data/images/ABC123.jpg",
-    ),
-    (
-        "ABC123",
-        "https://site.com/img/test.jpeg",
-        "data/images/ABC123.jpeg",
-    ),
-    (
-        "ABC123",
-        "",
-        "data/images/ABC123.bin",
-    ),
-]
+    cases = [
+        (
+            "FB-1812",
+            "https://site.com/images/FB-1812.webp",
+            "data/images/FB-1812.webp",
+        ),
+        (
+            "FB-1800-AZ",
+            "https://site.com/a/b/c.png",
+            "data/images/FB-1800-AZ.png",
+        ),
+        (
+            "ABC123",
+            "https://site.com/img/test.jpg",
+            "data/images/ABC123.jpg",
+        ),
+        (
+            "ABC123",
+            "https://site.com/img/test.jpeg",
+            "data/images/ABC123.jpeg",
+        ),
+        (
+            "ABC123",
+            "",
+            "data/images/ABC123.bin",
+        ),
+    ]
 
-
-print("=" * 80)
-print("IMAGE NAMER")
-print("=" * 80)
-
-for code, url, expected in tests:
-    result = namer.build(
-        code,
-        url,
-    )
-
-    print(result)
-
-    assert result == expected
-
-print()
-print("OK")
+    for code, url, expected in cases:
+        assert namer.build(code, url) == expected

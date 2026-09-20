84fa375d680e2438df3538e279daad00d75f37a4
impomerchperu <impomerch@gmail.com>
2026-09-20 15:05:03 -0500

    fix(images): satisfy ruff test fakes

--- tests/scraping/unit/test_image_sync_process.py
+++ tests/scraping/unit/test_image_sync_process.py
@@ -19,7 +19,6 @@ class FakeImageManager:
 class EmptyRepository:
     def find(self, code, image_url=None):
         del code, image_url
-        return None
 
 
 def test_image_sync_processes_products():

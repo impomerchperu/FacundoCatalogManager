280c32f94f7af90c0a6495ba442d780ae64ea72d
Cesar Sanchez <impomerchgmail.com>
2026-09-20 17:54:13 -0500

    style: separate cleanup import from module constants

--- services/scraping/scraping_factory.py
+++ services/scraping/scraping_factory.py
@@ -43,6 +43,7 @@ from services.scraping.scraping_config import ScrapingConfig
 from services.scraping.scraping_result_writer import ScrapingResultWriter
 from services.scraping.scraping_runner import ScrapingRunner
 from tools.clean_unused_images import clean_unused_images
+
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 

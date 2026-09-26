9cb3db10120c39677294343ef328406ce812b509
impomerchperu <impomerch@gmail.com>
2026-09-26 13:49:40 -0500

    test(app): cover application window icon setup

--- tests/test_app_startup.py
+++ tests/test_app_startup.py
@@ -10,6 +10,9 @@ def test_app_main_shows_window_before_entering_event_loop(monkeypatch):
         def __init__(self, argv):
             events.append(("application", argv))
 
+        def setWindowIcon(self, icon):
+            events.append(("icon", icon))
+
         def exec(self):
             events.append(("exec",))
             return 0
@@ -31,6 +34,7 @@ def test_app_main_shows_window_before_entering_event_loop(monkeypatch):
     assert exc_info.value.code == 0
     assert [entry[0] for entry in events] == [
         "application",
+        "icon",
         "window",
         "show",
         "exec",

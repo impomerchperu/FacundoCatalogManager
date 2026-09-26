d653785b208556cf81e60d2afb6150d90b0ee416
impomerchperu <impomerch@gmail.com>
2026-09-26 13:53:48 -0500

    test(scraping): ignore concurrent JSF request ordering in flaky case

--- tests/test_category_scraper.py
+++ tests/test_category_scraper.py
@@ -348,7 +348,7 @@ def test_category_scraper_continues_real_products_when_jsf_reports_one_page():
         f"{category_url.rstrip('/')}?product-page=2",
         f"{category_url.rstrip('/')}?product-page=3",
     ]
-    assert [
+    assert sorted(
         next(value for key, value in data if key == "paged")
         for _, data in browser.post_calls
-    ] == sorted(["1", "2", "3", "4"])
+    ) == sorted(["1", "2", "3", "4"])
